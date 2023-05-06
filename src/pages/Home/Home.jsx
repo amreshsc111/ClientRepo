604c625f2b0a2821c7d8ade78263c7654e87f017
Amresh Kumar Chaudhary <44799280+amreshsc111@users.noreply.github.com>
2023-05-06 11:36:17 +0545

    Added a button and footer

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -39,6 +39,12 @@ const Home = () => {
 	            </div>
 	        </div>
 	    </section>
+            {/* Footer container */}
+		<footer className='bg-neutral-100 text-center text-neutral-600 dark:bg-neutral-600 dark:text-neutral-200 lg:text-left'>
+			<div className='bg-neutral-200 p-6 text-center dark:bg-neutral-700'>
+				<span>© 2023 Copyright:Land Docket</span>
+			</div>
+		</footer>
     )
 }
 
