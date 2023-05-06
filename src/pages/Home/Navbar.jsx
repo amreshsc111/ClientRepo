b789db34fa32eeec66943e8a1a59d753829452fb
Amresh Kumar Chaudhary <44799280+amreshsc111@users.noreply.github.com>
2023-05-06 11:23:11 +0545

    Changed the styling of navbar

--- src/pages/Home/Navbar.jsx
+++ src/pages/Home/Navbar.jsx
@@ -4,14 +4,14 @@ import { nav } from '../../data';
 	
 const Nav = () => {
   return (
-    <nav>
+    <nav className="flex items-center justify-between flex-wrap p-6">
 	    <ul className='flex gap-x-10'>
 	      {nav.map((item, index) => {
 	        // destructure item
 	        const { href, name } = item;
 	        return (
 	          <li key={index}>
-	            <a className='hover:text-accent transition' href={href}>
+	            <a className='text-inherit hover:text-orange-500 hover:shadow-lg hover:no-underline transition' href={href}>
 	              {name}
 	            </a>
 	          </li>
